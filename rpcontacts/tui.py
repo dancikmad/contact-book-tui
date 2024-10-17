401fa8cb3709664ce3d0241c02e5941964444a59
Daniel Crigan <crgdaniel@yahoo.com>
2024-10-17 15:07:09 +0100

    added actions to the app's skeleton TUI: tui.py

--- rpcontacts/tui.py
+++ rpcontacts/tui.py
@@ -2,12 +2,25 @@ from textual.app import App
 from textual.widgets import Footer, Header
 
 class ContactsApp(App):
+    BINDINGS = [
+        ("m", "toggle_dark", "Toggle dark mode")
+    ]
     def compose(self):
+        """
+        Method to build the app's main screen
+        """
         yield Header()
         yield Footer()
 
     def on_mount(self):
+        """
+        Method to set up some properties of the main screen, like title and subtitle
+        """
         self.title = 'RP Contacts'
         self.sub_title = "A Contacts Book App with Textual & Python"
 
-        
+    def action_toggle_dark(self):
+        """
+        Method that the app must invoke when pressing the action key.
+        """
+        self.dark = not self.dark
