a1dc5348dde6c356cded4c7fb9f39e8df2211c50
Daniel Crigan <crgdaniel@yahoo.com>
2024-10-17 15:52:35 +0100

    built the contact's book tui

--- rpcontacts/tui.py
+++ rpcontacts/tui.py
@@ -1,19 +1,40 @@
 from textual.app import App
-from textual.containers import Grid
+from textual.containers import Grid, Horizontal, Vertical
 from textual.screen import Screen
-from textual.widgets import Footer, Header, Button, Label
+from textual.widgets import Footer, Header, Button, Label, DataTable, \
+    Static
 
 class ContactsApp(App):
     CSS_PATH = "rpcontacts.tcss"
     BINDINGS = [
         ("m", "toggle_dark", "Toggle dark mode"),
+        ("a", "add", "Add"),
+        ("q", "request_quit", "Quit"),
+        ("d", "delete", "Delete"),
+        ("c", "clear_all", "Clear All"),
         ("q", "request_quit", "Quit"),
     ]
+
     def compose(self):
         """
         Method to build the app's main screen
         """
         yield Header()
+        contacts_list = DataTable(classes="contacts-list")
+        contacts_list.focus()
+        contacts_list.add_columns("Name", "Phone", "Email")
+        contacts_list.cursor_type = "row"
+        contacts_list.zebra_stripes = True
+        add_button = Button("Add", variant="success", id="add")
+        add_button.focus()
+        buttons_panel = Vertical(
+            add_button,
+            Button("Delete", variant="warning", id="delete"),
+            Static(classes="separator"),
+            Button("Clear All", variant="error", id="clear"),
+            classes="buttons_panel"
+        )
+        yield Horizontal(contacts_list, buttons_panel)
         yield Footer()
 
     def on_mount(self):
